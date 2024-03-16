d4b44669b109fe5360125abc33e253bb396f1a8c
Mikel Gojenola <mgojenola002@ikasle.ehu.eus>
2024-03-17 00:31:01 +0100

    - Notificación y intent externo - Rotación gestionada en pantallas sin recyclerview

--- app/src/main/java/com/example/miapp/MainActivity.java
+++ app/src/main/java/com/example/miapp/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.miapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.content.pm.PackageManager;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +33,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setSupportActionBar(findViewById(R.id.labarra));
+        Toolbar barra = findViewById(R.id.labarra);
+        barra.setBackgroundColor(Color.MAGENTA);
+        setSupportActionBar(barra);
+
+        ImageView imagen = findViewById(R.id.imageView2);
+        imagen.setImageResource(R.drawable.imgportada);
+
+        Button pagina = findViewById(R.id.boton_pagina);
+        pagina.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createNotification();
+            }
+        });
+
+        Bundle extras=getIntent().getExtras();
+        if (extras!=null){
+            int id=extras.getInt("id");
+            NotificationManager elManager = (NotificationManager)
+                    getSystemService(Context.NOTIFICATION_SERVICE);
+            elManager.cancel(id);
+        }
 
         AdminDB adb = AdminDB.getMiADB(this, 1);
 
@@ -33,14 +71,10 @@ public class MainActivity extends AppCompatActivity {
         int id=item.getItemId();
         if(id == R.id.opcion_col){
             Intent i = new Intent (this, ColeccionActivity.class);
-            /*i.putExtra("nombre1",valor);
-            i.putExtra("nombre2", valor2);*/
             startActivity(i);
         }
         else if(id == R.id.opcion_tienda){
             Intent i = new Intent (this, TiendaActivity.class);
-            /*i.putExtra("nombre1",valor);
-            i.putExtra("nombre2", valor2);*/
             startActivity(i);
         }
         else if(id == R.id.opcion_plantilla){
@@ -48,5 +82,50 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    private void createNotification(){
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS)!= PackageManager.PERMISSION_GRANTED) {
+            //PEDIR EL PERMISO
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.POST_NOTIFICATIONS}, 11);
+        }
+
+        NotificationManager elManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder elBuilder = new NotificationCompat.Builder(this, "IdCanal");
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel elCanal = new NotificationChannel("IdCanal", "NombreCanal", NotificationManager.IMPORTANCE_DEFAULT);
+            //Configuración del canal
+            elCanal.setDescription("Descripción del canal");
+            elCanal.enableLights(true);
+            elCanal.setLightColor(Color.RED);
+            elCanal.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+            elCanal.enableVibration(true);
+
+            elManager.createNotificationChannel(elCanal);
+        }
+
+        Intent volver = new Intent (this, MainActivity.class);
+        volver.putExtra("id",1);
+        PendingIntent intentVolver = PendingIntent.getActivity(this, 0, volver,
+                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+
+        Intent salir = new Intent(Intent.ACTION_VIEW, Uri.parse("https://universe.leagueoflegends.com/es_AR/champions/"));
+        PendingIntent intentSalir = PendingIntent.getActivity(this, 0, salir,
+                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+
+        elBuilder.setLargeIcon(BitmapFactory.decodeResource(getResources(),R.drawable.ahri))
+                .setSmallIcon(android.R.drawable.stat_sys_warning)
+                .setContentTitle("Salir de la aplicación")
+                .setContentText("Esta acción te sacará de la aplicación")
+                .setSubText("Información extra")
+                .setVibrate(new long[]{0, 1000, 500, 1000})
+                .setAutoCancel(true)
+                .addAction(android.R.drawable.sym_action_call,"Volver",intentVolver)
+                .addAction(android.R.drawable.sym_action_call,"Continuar",intentSalir);
+
+        elManager.notify(1, elBuilder.build());
+    }
+
 
 }
